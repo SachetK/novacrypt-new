da503a3e83c0ce17fc43a0f8f1e7e81ea645bb52
SachetK <71898487+SachetK@users.noreply.github.com>
2023-05-26 13:33:48 -0400

    images

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { type NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
 import animatedLogo from "public/mainlogo.gif";
 
 const Home: NextPage = () => {
@@ -36,25 +37,27 @@ const Home: NextPage = () => {
             />
             <section className="absolute inset-0 left-[13%] right-[13%] grid h-5/6 w-3/4 grid-cols-2 place-content-center gap-4 group-hover/main:place-content-between">
               <div className="group/item opacity-0 group-hover/main:place-self-start group-hover/main:opacity-100 group-hover/main:delay-500 group-hover/main:duration-200">
-                <div className="relative grid h-48 w-48 place-items-center rounded-full bg-violet-600 transition-all duration-500 ease-in-out group-hover/item:scale-110 group-hover/item:bg-violet-800">
-                  <h3 className="text-center text-3xl font-bold text-white ">
+                <div className="relative z-10 grid h-48 w-48 place-items-center rounded-full bg-violet-600 transition-all duration-500 ease-in-out group-hover/item:scale-110 group-hover/item:bg-violet-800">
+                  <h3 className="z-auto text-center text-3xl font-bold text-white ">
                     Support from
                   </h3>
-                  {supports.map((support, idx) => {
-                    return (
-                      <div key={idx} className="absolute inset-0 h-16 w-16">
-                        <Image
-                          src={`/support/${support.toLowerCase()}.png`}
-                          fill
-                          alt={support}
-                        />
-                      </div>
-                    );
-                  })}
+                  <div className="absolute grid place-content-center">
+                    {supports.map((support, idx) => {
+                      return (
+                        <div className="absolute h-16 w-16" key={idx}>
+                          <Image
+                            src={`/support/${support.toLowerCase()}.png`}
+                            fill
+                            alt={support}
+                          />
+                        </div>
+                      );
+                    })}
+                  </div>
                 </div>
               </div>
               <a
-              href="https://www.gofundme.com"
+                href="https://www.gofundme.com"
                 className="group/item opacity-0 group-hover/main:place-self-end group-hover/main:opacity-100 group-hover/main:delay-500 group-hover/main:duration-200"
               >
                 <div className="grid h-48 w-48 place-items-center rounded-full bg-violet-600 transition-all duration-500 ease-in-out group-hover/item:scale-110 group-hover/item:bg-violet-800">
@@ -64,11 +67,14 @@ const Home: NextPage = () => {
                 </div>
               </a>
               <div className="group/item  col-span-2 place-self-center opacity-0 group-hover/main:opacity-100 group-hover/main:delay-500 group-hover/main:duration-200">
-                <div className="grid h-48 w-48 place-items-center rounded-full bg-violet-600 transition-all duration-500 ease-in-out group-hover/item:scale-110 group-hover/item:bg-violet-800">
+                <Link
+                  href="/team"
+                  className="grid h-48 w-48 place-items-center rounded-full bg-violet-600 transition-all duration-500 ease-in-out group-hover/item:scale-110 group-hover/item:bg-violet-800"
+                >
                   <h3 className="p-4 text-center text-3xl font-bold text-white">
                     Who are we?
                   </h3>
-                </div>
+                </Link>
               </div>
             </section>
           </div>
--- src/pages/team.tsx
+++ src/pages/team.tsx
@@ -1,10 +1,66 @@
 import type { NextPage } from "next";
+import Image from "next/image";
+import Link from "next/link";
 
 const TeamPage: NextPage = () => {
+  const supports = [
+    "Cambridge",
+    "Columbia",
+    "Harvard",
+    "IBM",
+    "IITs",
+    "ISEF",
+    "MIT",
+    "NASA",
+    "Stanford",
+    "UN",
+  ];
+
   return (
-    <div className="h-full min-h-screen overflow-hidden bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-      <h1>Team</h1>
-    </div>
+    <main className="flex h-full min-h-screen flex-col items-center justify-center overflow-hidden bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+      <section className="absolute inset-0 top-96 left-[13%] right-[13%] grid h-5/6 w-3/4 grid-cols-2 place-content-between gap-4">
+        <div className="group/item place-self-start opacity-100 group-hover/main:opacity-100 group-hover/main:delay-500 group-hover/main:duration-200">
+          <div className="relative z-10 grid h-48 w-48 place-items-center rounded-full bg-violet-600 transition-all duration-500 ease-in-out group-hover/item:scale-110 group-hover/item:bg-violet-800">
+            <h3 className="relative z-auto text-center text-3xl font-bold text-white ">
+              Support from
+            </h3>
+            <div className="grid grid-cols-2 place-content-center w-full h-full">
+              {supports.map((support, idx) => {
+                return (
+                    <Image
+                      src={`/support/${support.toLowerCase()}.png`}
+                      height={64}
+                    width={64}
+                      alt={support}
+                        key={idx}
+                    />
+                );
+              })}
+            </div>
+          </div>
+        </div>
+        <a
+          href="https://www.gofundme.com"
+          className="group/item opacity-0 group-hover/main:place-self-end group-hover/main:opacity-100 group-hover/main:delay-500 group-hover/main:duration-200"
+        >
+          <div className="grid h-48 w-48 place-items-center rounded-full bg-violet-600 transition-all duration-500 ease-in-out group-hover/item:scale-110 group-hover/item:bg-violet-800">
+            <h3 className="text-center text-3xl font-bold text-white">
+              Donate
+            </h3>
+          </div>
+        </a>
+        <div className="group/item  col-span-2 place-self-center opacity-0 group-hover/main:opacity-100 group-hover/main:delay-500 group-hover/main:duration-200">
+          <Link
+            href="/team"
+            className="grid h-48 w-48 place-items-center rounded-full bg-violet-600 transition-all duration-500 ease-in-out group-hover/item:scale-110 group-hover/item:bg-violet-800"
+          >
+            <h3 className="p-4 text-center text-3xl font-bold text-white">
+              Who are we?
+            </h3>
+          </Link>
+        </div>
+      </section>
+    </main>
   );
 };
 
