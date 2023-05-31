21e7ee4482ae93a3930249d32a5b7d0312bb27c0
SachetK <71898487+SachetK@users.noreply.github.com>
2023-05-30 23:39:09 -0400

    Meta for Team

--- src/pages/team.tsx
+++ src/pages/team.tsx
@@ -1,10 +1,15 @@
 import type { NextPage } from "next";
+import Head from "next/head";
 
 const TeamPage: NextPage = () => {
  
   return (
     <main className="flex h-full min-h-screen flex-col items-center justify-center overflow-hidden bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-      
+      <Head>
+        <title>NovaCrypt | Team</title>
+        <meta name="description" content="Team page for NovaCrypt" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
     </main>
   );
 };
